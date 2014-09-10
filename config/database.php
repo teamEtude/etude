f41e05014779ca4056d14ed1e46f322efa559630
schooljack <s3449789@student.rmit.edu.au>
2014-09-10 04:42:37 +0000

    First commit!

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $etude = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'admin',
+		'password' => 'somethinghard',
+		'database' => 'etude',
+	);
+}
+?>
